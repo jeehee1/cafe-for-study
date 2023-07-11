3979e11c68af7a1e3ce4debf53940fae83898547
jeehee1 <jeehee1w@gmail.com>
2023-07-11 10:27:23 +0900

    feat: 필터 클릭시 필터 버튼 액티브 효과 생성
    카페 리스트 페이지에서 필터를 클릭하면 필터 버튼이 어두워지며 필터가 클릭되었음을 표시

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootPage from "./pages/Root";
 import HomePage from "./pages/Home";
+import ShowShops from "./components/shops/ShowShops";
+import ShopsList from "./pages/ShopsList";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootPage />,
     id: "root",
-    children: [{ index: true, element: <HomePage /> }],
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "coffeeshops", element:<ShopsList/> },
+    ],
   },
 ]);
 
